667cdebff600f2a780738c6da5a1678179787009
Wildyao <z8927623@163.com>
2014-12-15 11:42:57 +0800

    First commit

---
+++ PodProjDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PodProjDemo
+//
+//  Created by wildyao on 14/12/15.
+//  Copyright (c) 2014年 Wild Yaoyao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
